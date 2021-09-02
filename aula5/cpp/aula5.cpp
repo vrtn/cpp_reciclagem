307b053935d2d1eb5dfa9ff0c5861a66acd1fc4b
ezaninibr <everton.zanini@yahoo.com>
2021-09-02 17:02:24 -0300

    Aula 7 de incremento terminada

---
+++ aula5/cpp/aula5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.1415
+#define CANAL cout << "Canal Fessor Bruno \n\n"
+
+int main(){
+	
+	int vidas = 3, tiros = 500, life = 100;
+	
+	cout << PI << "\n\n";
+	
+	CANAL;
+	
+	return 0;
+}
---
+++ aula6/cpp/aula6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n1, n2; // Variávies Globais
+
+int main(){
+	
+	int n3, n4; // Variáveis locais
+	int res;
+	
+	n1 = 11;
+	n2 = 3;
+	n3 = 5;
+	n4 = 2;
+	
+	res = n1+n2*n4;
+	
+	cout << "Soma de todas os valores: " << res << "\n\n";
+	
+	return 0;
+}
